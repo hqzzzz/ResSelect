b41a32814984d1c3d4691eaa2da4c8f5eb0f1402
yang.linhao <yang.linhao@hotmail.com>
2020-12-16 15:58:14 +0800

    Init

---
+++ UitRes/UitRES.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace UitRes
+{
+    partial class UitRES
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_s = new System.Windows.Forms.Button();
+            this.CB_Nc = new System.Windows.Forms.ComboBox();
+            this.TB_Rop = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 87);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 351);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btn_s
+            // 
+            this.btn_s.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_s.Font = new System.Drawing.Font("宋体", 18F);
+            this.btn_s.Location = new System.Drawing.Point(633, 25);
+            this.btn_s.Name = "btn_s";
+            this.btn_s.Size = new System.Drawing.Size(155, 56);
+            this.btn_s.TabIndex = 1;
+            this.btn_s.Text = "START";
+            this.btn_s.UseVisualStyleBackColor = true;
+            this.btn_s.Click += new System.EventHandler(this.btn_s_Click);
+            // 
+            // CB_Nc
+            // 
+            this.CB_Nc.Font = new System.Drawing.Font("宋体", 18F);
+            this.CB_Nc.FormattingEnabled = true;
+            this.CB_Nc.Location = new System.Drawing.Point(434, 29);
+            this.CB_Nc.Name = "CB_Nc";
+            this.CB_Nc.Size = new System.Drawing.Size(121, 32);
+            this.CB_Nc.TabIndex = 2;
+            // 
+            // TB_Rop
+            // 
+            this.TB_Rop.Font = new System.Drawing.Font("宋体", 18F);
+            this.TB_Rop.Location = new System.Drawing.Point(170, 26);
+            this.TB_Rop.Name = "TB_Rop";
+            this.TB_Rop.Size = new System.Drawing.Size(100, 35);
+            this.TB_Rop.TabIndex = 3;
+            this.TB_Rop.Text = "3.095";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 20F);
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 27);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Proportion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 20F);
+            this.label2.Location = new System.Drawing.Point(322, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 27);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ncount";
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // UitRES
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TB_Rop);
+            this.Controls.Add(this.CB_Nc);
+            this.Controls.Add(this.btn_s);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "UitRES";
+            this.Text = "UitRES";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_s;
+        private System.Windows.Forms.ComboBox CB_Nc;
+        private System.Windows.Forms.TextBox TB_Rop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+    }
+}
+
---
+++ UitRes/UitRES.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UitRes
+{
+    public partial class UitRES : Form
+    {
+
+
+        public class ResA
+        {
+
+            public double R0 { get; set; }
+            public double R1 { get; set; }
+            public double R2 { get; set; }
+            public double pRop { get; set; }
+
+            public double dif { get; set; }
+
+
+        }
+        double[] Reslist = {1,2,2.2,4.7,5.1,10,15,20,22,24,27,33,47,49.9,51,56,68,75,82,91,
+            100,110,120,130,150,160,180,200,220,240,270,300,330,360,390,430,470,510,560,620,680,750,820,910,
+            1000,1100,1200,1500,1800,2000,2200,2400,2700,3000,3300,3600,3900,4300,4700,4990,5100,5600,6200,6800,7500,8200,9100,
+            10000,11000,12000,13000,15000,16000,18000,20000,22000,24000,27000,30000,33000,36000,39000,40200,43000,47000,49900,51000,56000,62000,68000,75000,82000,91000,
+            1e+05,1.1e+05,1.2e+05,1.3e+05,1.5e+05,1.6e+05,1.8e+05,2e+05,2.2e+05,2.4e+05,2.7e+05,3e+05,3.3e+05,3.6e+05,3.9e+05,4.3e+05,4.7e+05,5.1e+05,5.6e+05,6.2e+05,6.8e+05,7.5e+05,
+            1e+06,1.2e+06,1.5e+06,2e+06,2.2e+06,3e+06,4.7e+06,5.1e+06,
+            1e+07
+            };
+
+        DataView dataView = new DataView();
+
+        List<ResA> ResLis = new List<ResA>();
+        public UitRES()
+        {
+            InitializeComponent();
+
+            CB_Nc.Items.Add(2);
+            CB_Nc.Items.Add(3);
+            CB_Nc.SelectedIndex = 0;
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+        }
+
+
+   
+   
+
+
+        private void btnslove2(double Rp)
+        {
+            double Rpmax = Rp * 1.002;
+            double Rpmin = Rp * (1.000 - 0.002);
+
+            ParallelLoopResult result = Parallel.ForEach<double>(Reslist, res =>
+            {
+
+                for (int i = 0; i < Reslist.Length; i++)
+                {
+                        double rop = (double)res /Reslist[i];
+                        double dif = (rop - Rp) / Rp;
+                        if (rop > Rpmin && rop < Rpmax)
+                        {
+                        ResLis.Add(new ResA { R1 = res, R2 = Reslist[i], pRop = rop, dif = dif });
+                        }
+
+                }
+            });
+
+            ResLis = ResLis.OrderBy(u => u.R1 + u.R0).ToList();
+
+            Console.WriteLine("是否完成:{0}", result.IsCompleted);
+            dataGridView1.DataSource = ResLis;
+            btn_s.Enabled = true;
+      
+        }
+
+
+
+        private void btnslove3(double Rp)
+        {
+           
+            double Rpmax = Rp * 1.001;
+            double Rpmin = Rp * (1.000 - 0.001);
+
+            for (int i = 0; i < Reslist.Length; i++)
+            {
+                ParallelLoopResult result = Parallel.ForEach<double>(Reslist, new ParallelOptions { MaxDegreeOfParallelism = 16 }, res =>
+             {
+
+                     for (int j = 0; j < Reslist.Length; j++)
+                     {
+                         double rop = (double)(res + Reslist[i]) /Reslist[j];
+                        double dif = (rop - Rp) / Rp;
+                     if (rop > Rpmin && rop < Rpmax)
+                         {
+                            ResLis.Add(new ResA { R0 = res, R1 = Reslist[i], R2 = Reslist[j], pRop = rop, dif = dif });
+                         }
+                     }
+                 
+                
+               });
+            }
+
+
+            ResLis= ResLis.OrderBy(u => u.R1+u.R0).ToList();
+
+            dataGridView1.DataSource = ResLis;
+            btn_s.Enabled = true;
+
+         
+        }
+
+
+        private void btn_s_Click(object sender, EventArgs e)
+        {
+            btn_s.Enabled = false;
+            double pRop = double.Parse(TB_Rop.Text);
+
+            int Nc = int.Parse(CB_Nc.Text);
+            ResLis.Clear();
+
+            if (Nc == 2)
+            {
+ 
+                btnslove2(pRop);
+
+            }
+            else if (Nc == 3)
+            {
+               
+                btnslove3(pRop);
+            }
+
+        }
+    }
+}
